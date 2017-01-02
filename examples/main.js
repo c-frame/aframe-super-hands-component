b465fde92285c922df50f0423cecc2ca23a13422
William Murphy <william@datatitian.com>
2017-01-01 22:26:09 -0600

    add first example, super-hands in a scene without physics

--- examples/main.js
+++ examples/main.js
@@ -1,2 +1,6 @@
 require('aframe');
 require('../index.js');
+require('aframe-extras').registerAll();
+require('aframe-event-set-component');
+//AFRAME.registerComponent('grid', extras.primitives.grid);
+//AFRAME.registerComponent('sphere-collider'. extras.misc['sphere-collider']);
