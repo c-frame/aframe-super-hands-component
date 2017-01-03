af9faec750991cd6f5065fa56a0c9776296d0820
William Murphy <william@datatitian.com>
2017-01-02 22:23:15 -0600

    remove superfluous files in gh-pages directory

--- gh-pages/main.js
+++
@@ -1,6 +0,0 @@
-require('aframe');
-require('../index.js');
-require('aframe-extras').registerAll();
-require('aframe-event-set-component');
-//AFRAME.registerComponent('grid', extras.primitives.grid);
-//AFRAME.registerComponent('sphere-collider'. extras.misc['sphere-collider']);
