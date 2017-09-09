d2e5fcc8b73707855e08bb839bec78359092f0f0
William Murphy <william@datatitian.com>
2017-09-09 17:14:57 -0500

    core code for button mapping

---
+++ reaction_components/buttons-proto.js
@@ -0,0 +1,19 @@
+// common code used in customizing reaction components by button
+module.exports = (function () {
+  function buttonIsValid (evt, buttonList) {
+    return buttonList.length === 0 ||
+        buttonList.indexOf(evt.detail.buttonEvent.type) !== -1;
+  }
+  return {
+    schema: {
+      startButtons: {default: []},
+      endButtons: {default: []}
+    },
+    startButtonOk: function (evt) {
+      return buttonIsValid(evt, this.data['startButtons']);
+    },
+    endButtonOk: function (evt) {
+      return buttonIsValid(evt, this.data['endButtons']);
+    }
+  };
+})();
