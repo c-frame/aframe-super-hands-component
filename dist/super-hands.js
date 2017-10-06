d8cf68688d18fb3bf8c586e85024e2730b9b41d0
William Murphy <william@datatitian.com>
2017-10-06 18:15:03 -0500

    make reusable setAttribute object not shared between grabbable instances

--- dist/super-hands.js
+++ dist/super-hands.js
@@ -642,6 +642,8 @@
 	    this.grabDistance = undefined;
 	    this.grabDirection = { x: 0, y: 0, z: -1 };
 	    this.grabOffset = { x: 0, y: 0, z: 0 };
+	    // persistent object speeds up repeat setAttribute calls
+	    this.destPosition = { x: 0, y: 0, z: 0 };
 	    this.physicsInit();
 
 	    this.el.addEventListener(this.GRAB_EVENT, function (e) {
@@ -663,7 +665,6 @@
 	  tick: function () {
 	    var deltaPosition = new THREE.Vector3();
 	    var targetPosition = new THREE.Vector3();
-	    var destPosition = { x: 0, y: 0, z: 0 };
 	    return function () {
 	      var entityPosition;
 	      if (this.grabber) {
@@ -674,10 +675,10 @@
 	          // relative position changes work better with nested entities
 	          deltaPosition.sub(targetPosition);
 	          entityPosition = this.el.getAttribute('position');
-	          destPosition.x = entityPosition.x - deltaPosition.x * this.xFactor;
-	          destPosition.y = entityPosition.y - deltaPosition.y * this.yFactor;
-	          destPosition.z = entityPosition.z - deltaPosition.z * this.zFactor;
-	          this.el.setAttribute('position', destPosition);
+	          this.destPosition.x = entityPosition.x - deltaPosition.x * this.xFactor;
+	          this.destPosition.y = entityPosition.y - deltaPosition.y * this.yFactor;
+	          this.destPosition.z = entityPosition.z - deltaPosition.z * this.zFactor;
+	          this.el.setAttribute('position', this.destPosition);
 	        } else {
 	          this.deltaPositionIsValid = true;
 	        }
