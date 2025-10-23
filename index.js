ed1ca7a7d3cb3d8e3633f631ab150eb007b6f65d
Leandro Melo <tpleme@hotmail.com>
2025-10-23 18:08:20 +0100

    updated dependencies version on ammo examples, added ammojs example link to examples main page

--- index.js
+++ index.js
@@ -288,8 +288,9 @@ AFRAME.registerComponent('super-hands', {
     const hoverEls = this.hoverEls
     const hitElIndex = this.hoverEls.indexOf(hitEl)
     let hoverNeedsUpdate = false
-    if (dist && intersection.instanceId !== undefined)
-      hitEl.object3D.userData = { instanceId: intersection.instanceId };
+    if (dist && intersection.instanceId !== undefined) {
+      hitEl.object3D.userData = { instanceId: intersection.instanceId }
+    }
     if (hitElIndex === -1) {
       hoverNeedsUpdate = true
       // insert in order of distance when available
